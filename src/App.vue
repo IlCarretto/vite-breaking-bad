02013ef9f26f88dfa7fdaa766e06ada40f958aef
IlCarretto <carlorebuffat412@gmail.com>
2022-11-10 17:04:41 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import axios from "axios";
+import AppMain from "./components/AppMain.vue";
+import {store} from "./store";
+
+export default {
+  components: {
+    AppMain,
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+<AppMain/>
+</template>
+
+<style lang="scss" scoped>
+@use "./styles/general.scss" as *;
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    name: "AppMain",
+    components: {
+
+    },
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<template>
+    <main>
+        <h1>Breaking Bad Api</h1>
+        <div class="ms-container">
+        </div>
+    </main>
+        
+</template>
+
+<style lang="scss" scoped>
+    main {
+        background-color: #2e3a46;
+    }
+</style>
